2798070b757d0cc69daba6ae75a5b995d5c2e5fb
yunuszanzi <yunuszanzi@gmail.com>
2024-11-25 12:15:04 +0500

    backend connection

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.get("/test", async (req: Request, res: Response) => {
+  res.json({ message: "test message" });
+});
+
+app.listen(7000, () => {
+  console.log("Server started on localhost:7000");
+});
